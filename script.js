9e53f052a564052267c75a34eab12a14f3023aae
Abhishek-DevSpace <AbhiWorkInbox@gmail.com>
2026-03-15 12:34:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple interaction: Alert on email click or smooth entrance
+document.querySelectorAll('.glass-card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        console.log("Exploring Abhishek's skills...");
+    });
+});
